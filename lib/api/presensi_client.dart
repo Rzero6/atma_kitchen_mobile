07cab2a36e615df3a2075ec8fa1d0b5c9f3bdada
Reynold <michaelreynoldk@gmail.com>
2024-05-11 06:06:41 +0700

    presensi view

--- lib/api/presensi_client.dart
+++ lib/api/presensi_client.dart
@@ -8,6 +8,31 @@ import 'package:shared_preferences/shared_preferences.dart';
 class PresensiClient {
   static final ApiClient apiClient = ApiClient();
   static const String timeout = 'Take too long, check your connection';
+  static Future<List<Presensi>> getAllPresensi() async {
+    var client = http.Client();
+    Uri uri;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token')!;
+
+    try {
+      uri = Uri.parse('${apiClient.baseUrl}/presensi');
+      var response = await client.get(uri, headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      }).timeout(const Duration(seconds: 30));
+      if (response.statusCode != 200) {
+        throw json.decode(response.body)['message'].toString();
+      }
+      Iterable list = json.decode(response.body)['data'];
+      return list.map((e) => Presensi.fromJson(e)).toList();
+    } on TimeoutException catch (_) {
+      return Future.error(timeout);
+    } catch (e) {
+      return Future.error(e.toString());
+    } finally {
+      client.close();
+    }
+  }
 
   static Future<List<Presensi>> getPresensisHariIni() async {
     var client = http.Client();
