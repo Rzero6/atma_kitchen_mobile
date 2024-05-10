3cfad3079ea47e59fd144b75c6ecb30a19a80ee5
Reynold <michaelreynoldk@gmail.com>
2024-05-10 22:52:49 +0700

    riwayat

--- lib/model/detail_transaksi.dart
+++ lib/model/detail_transaksi.dart
@@ -1,16 +1,24 @@
+import 'package:atma_kitchen_mobile/model/hampers.dart';
+import 'package:atma_kitchen_mobile/model/produk.dart';
+
 class DetailTransaksi {
   int? id;
   int? idTransaksi;
   int? idProduk;
   int? idHampers;
   int jumlah;
+  Produk? produk;
+  Hampers? hampers;
 
-  DetailTransaksi(
-      {this.id,
-      this.idTransaksi,
-      this.idProduk,
-      this.idHampers,
-      required this.jumlah});
+  DetailTransaksi({
+    this.id,
+    this.idTransaksi,
+    this.idProduk,
+    this.idHampers,
+    required this.jumlah,
+    required this.produk,
+    required this.hampers,
+  });
 
   factory DetailTransaksi.fromJson(Map<String, dynamic> json) {
     return DetailTransaksi(
@@ -19,6 +27,9 @@ class DetailTransaksi {
       idProduk: json['id_produk'],
       idHampers: json['id_hampers'],
       jumlah: json['jumlah'],
+      produk: json['produk'] != null ? Produk.fromJson(json['produk']) : null,
+      hampers:
+          json['hampers'] != null ? Hampers.fromJson(json['hampers']) : null,
     );
   }
 
